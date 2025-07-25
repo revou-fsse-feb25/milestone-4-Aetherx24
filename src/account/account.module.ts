d376be96ccf12e2d57786440632fc7f31f89fc2e
Aetherx24 <iqbalfahrur@gmail.com>
2025-07-26 01:06:06 +0700

    feat: Importedr PrismaModule

--- src/account/account.module.ts
+++ src/account/account.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AccountController } from './account.controller';
 import { AccountService } from './account.service';
+import { PrismaModule } from '../prisma/prisma.module'
 
 @Module({
+  imports: [PrismaModule],
   controllers: [AccountController],
   providers: [AccountService]
 })
