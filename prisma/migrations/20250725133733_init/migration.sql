caaae997004d3b3790ea9daa17a5f97c7b07511d
Aetherx24 <iqbalfahrur@gmail.com>
2025-07-25 20:38:43 +0700

    feat: migrate prisma to db

---
+++ prisma/migrations/20250725133733_init/migration.sql
@@ -0,0 +1,22 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Account` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Transaction` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Account" DROP CONSTRAINT "Account_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Transaction" DROP CONSTRAINT "Transaction_accountId_fkey";
+
+-- DropTable
+DROP TABLE "Account";
+
+-- DropTable
+DROP TABLE "Transaction";
+
+-- DropTable
+DROP TABLE "User";
