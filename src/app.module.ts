9f3d3193efd3df1f23ad50dac109532e57f7f1c7
Aetherx24 <iqbalfahrur@gmail.com>
2025-07-25 20:39:03 +0700

    initial commit NestJS modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { AccountModule } from './account/account.module';
+import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, AuthModule, UserModule, AccountModule, TransactionModule],
   controllers: [AppController],
   providers: [AppService],
 })
