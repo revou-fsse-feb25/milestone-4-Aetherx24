d359379b2a800469a333ea17a05e0d89bd7ca0ec
Aetherx24 <iqbalfahrur@gmail.com>
2025-08-02 12:57:19 +0700

    feat: fixed validation

--- src/user/dto/user-id.dto.ts
+++ src/user/dto/user-id.dto.ts
@@ -1,5 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNumberString, IsPositive } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class UserIdDto {
   @ApiProperty({
@@ -8,5 +9,6 @@ export class UserIdDto {
   })
   @IsNumberString()
   @IsPositive()
+  @Transform(({ value }) => parseInt(value, 10))
   id: string;
 } 
